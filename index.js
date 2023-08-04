0ad7f5ad2ec79a6f0224635bc3be66f3cca61abd
Adam <adamsoliman800@gmail.com>
2023-08-04 15:21:11 +0300

    TolapyServer

--- index.js
+++ index.js
@@ -188,4 +188,8 @@ app.delete('/group-attendances/:id', (req, res) => {
 app.listen(3001, () => {
     console.log('Express server listening on port 3001');
 });
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 module.exports = app;
