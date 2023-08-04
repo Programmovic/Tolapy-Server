316ca9fec25c7bc5910eb92a6a14e629b0c3d59f
Adam <adamsoliman800@gmail.com>
2023-08-04 15:35:06 +0300

    last

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const Teacher = require('./models/teacher');
 const Student = require('./models/student');
@@ -19,6 +20,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 // =============================================================================
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 // =============================================================================
 // //REGISTER TEACHER API
 // =============================================================================
