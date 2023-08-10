9667515731c47be945b1ac637bbcc2c19ff9fec9
Adam <adamsoliman800@gmail.com>
2023-08-10 17:02:58 +0300

    update getNextStageId.js

--- utils/getNextStageId.js
+++ utils/getNextStageId.js
@@ -2,7 +2,7 @@ const Stage = require('../models/stage');
 // =============================================================================
 // // Helper function to get the next ID for a stage
 // =============================================================================
-export async function getNextStageId() {
+module.exports = async function getNextStageId() {
     const lastStage = await Stage.findOne().sort({ id: -1 });
     if (!lastStage) {
         return 1;
