8f3968fc683c31101695bec8fe79969d684f5080
Adam <adamsoliman800@gmail.com>
2023-08-25 23:28:44 +0300

    update student.js

--- models/student.js
+++ models/student.js
@@ -31,6 +31,7 @@ const studentSchema = new mongoose.Schema({
   studentAttendances: { type: [Attendance.schema] },
   studentSubjects: { type: [mongoose.Schema.Types.Mixed] },
   isSend: { type: Boolean, default: true },
+  isArchived: { type: Boolean, default: false },
   created_at: { type: Date, default: Date.now }
 }, { collection: 'Students' });
 
